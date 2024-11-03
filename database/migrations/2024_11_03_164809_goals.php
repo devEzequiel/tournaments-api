0bd3a459ab361b9d68e9aae73dd8167b7d692b3a
Ezequiel Oliveira <esofinney@gmail.com>
2024-11-03 14:19:37 -0300

    created awards table

--- database/migrations/2024_11_03_164809_goals.php
+++ database/migrations/2024_11_03_164809_goals.php
@@ -15,11 +15,11 @@ return new class extends Migration {
         Schema::create('fixtures', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('fixture_id');
-            $table->unsignedBigInteger('championship_id');
             $table->unsignedBigInteger('scorer_id');
             $table->unsignedBigInteger('assist_id');
 
-            $table->foreign('championship_id')->references('id')->on('championships');
+            $table->boolean('pk')->default(false);
+
             $table->foreign('fixture_id')->references('id')->on('fixtures');
             $table->foreign('scorer_id')->references('id')->on('players');
             $table->foreign('assist_id')->references('id')->on('players');
---
+++ database/migrations/2024_11_03_170906_awards.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fixtures', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('first_place'); //champion
+            $table->unsignedBigInteger('second_place'); //vice
+            $table->unsignedBigInteger('third_place'); //bronze
+
+            $table->unsignedBigInteger('best_player'); //player of the championship: biggest rate
+            $table->unsignedBigInteger('golden_boot'); //most goals
+            $table->unsignedBigInteger('playmaker'); //most assists
+            $table->unsignedBigInteger('golden_glove'); //fewer goals conceded
+
+            $table->foreign('first_place_id')->references('id')->on('teams');
+            $table->foreign('second_place_id')->references('id')->on('teams');
+            $table->foreign('third_place_id')->references('id')->on('teams');
+
+            $table->foreign('best_player')->references('id')->on('players');
+            $table->foreign('golden_boot')->references('id')->on('players');
+            $table->foreign('playmaker')->references('id')->on('players');
+            $table->foreign('golden_glove')->references('id')->on('players');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};

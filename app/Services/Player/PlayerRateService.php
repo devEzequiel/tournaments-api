4c968c33faf5400b858675fedd7494dbe029b004
Ezequiel Oliveira <esofinney@gmail.com>
2024-11-24 22:54:23 -0300

    added service

--- app/Services/Player/PlayerRateService.php
+++ app/Services/Player/PlayerRateService.php
@@ -2,10 +2,28 @@
 
 namespace App\Services\Player;
 
-class PlayerRateService
+use App\Contracts\PlayerRateContract;
+use App\Models\PlayerRate;
+use App\Services\BaseService;
+
+class PlayerRateService extends BaseService implements PlayerRateContract
 {
 
-    public function create($data)
+    public function __construct()
     {
+        parent::__construct(new PlayerRate());
+    }
+
+    public function create($data): bool
+    {
+        $player = $this->model::create($data);
+        $pivot_data = [
+            'team_id' => $data['team_id'],
+            'player_id' => $player->id,
+            'current_team' => true,
+            'joined_at' => now()
+        ];
+        TeamPlayer::create($pivot_data);
+        return true;
     }
 }

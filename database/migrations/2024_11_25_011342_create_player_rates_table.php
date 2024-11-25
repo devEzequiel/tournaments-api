2571c3f8c2d8f8079dea131a84a9d315869856f4
Ezequiel Oliveira <esofinney@gmail.com>
2024-11-24 22:30:30 -0300

    created rate method

---
+++ database/migrations/2024_11_25_011342_create_player_rates_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('player_rates', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('fixture_id');
+            $table->unsignedBigInteger('player_id');
+            $table->float('rate');
+
+
+            $table->foreign('championship_id')->references('id')->on('championships');
+            $table->foreign('player_id')->references('id')->on('players');
+            $table->foreign('fixture_id')->references('id')->on('fixtures');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rates');
+    }
+};

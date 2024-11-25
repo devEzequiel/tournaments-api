d429f322e5549a05606270a0eabd02d9af59123a
Ezequiel Oliveira <esofinney@gmail.com>
2024-11-24 22:12:16 -0300

    created services

--- app/Http/Controllers/Analytics/TeamAnalyticController.php
+++ app/Http/Controllers/Analytics/TeamAnalyticController.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Controllers\Analytics;
 
-class TeamAnalyticController
-{
+use App\Http\Controllers\Controller;
 
+class TeamAnalyticController extends Controller
+{
+    public function currentPlayersData(int $id)
+    {
+        try {
+            $team = $this->service->getCurrentPlayersData($id);
+
+            return $this->responseOk($team);
+        } catch (\Exception $e) {
+            return $this->responseUnprocessableEntity($e->getMessage());
+        }
+    }
 }

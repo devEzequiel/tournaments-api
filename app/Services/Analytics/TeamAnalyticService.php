2c6826b77e97984040cc59d6805a856924191670
Ezequiel Oliveira <esofinney@gmail.com>
2024-11-24 22:12:19 -0300

    created services

--- app/Services/Analytics/TeamAnalyticService.php
+++ app/Services/Analytics/TeamAnalyticService.php
@@ -2,7 +2,24 @@
 
 namespace App\Services\Analytics;
 
-class TeamAnalyticService
-{
+use App\Models\Team;
+use App\Services\BaseService;
 
+class TeamAnalyticService extends BaseService implements TeamAnalyticContract
+{
+    public function __construct()
+    {
+        parent::__construct(new Team());
+    }
+    public function getCurrentPlayersData(int $id)
+    {
+        $team_players_data = $this->model::query()->where('team_id', $id)
+            ->get();
+
+        if (!$team_players) {
+            throw new Exception('Nenhum jogador encontrado');
+        }
+
+        return $team_players;
+    }
 }

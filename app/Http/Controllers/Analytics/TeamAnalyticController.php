a6211733c424f9935c32342dc2cc6498f8f9b53e
Ezequiel Oliveira <esofinney@gmail.com>
2024-12-08 02:11:03 -0300

    Add player data retrieval methods to TeamAnalyticController

--- app/Http/Controllers/Analytics/TeamAnalyticController.php
+++ app/Http/Controllers/Analytics/TeamAnalyticController.php
@@ -3,15 +3,55 @@
 namespace App\Http\Controllers\Analytics;
 
 use App\Http\Controllers\Controller;
+use App\Services\Analytics\TeamAnalyticService;
+use Illuminate\Http\JsonResponse;
 
 class TeamAnalyticController extends Controller
 {
+
+    public function __construct(protected TeamAnalyticService $service)
+    {
+    }
+
+    /**
+     * Fetches current player data for a specified team ID.
+     *
+     * This function aims to retrieve the data of current team players using the given
+     * team ID. Upon success, it returns a successful response with the relevant
+     * players data. If an exception is encountered, it manages the error by returning
+     * an unprocessable entity response along with the exception message.
+     *
+     * @param int $id The team ID for which current player data is to be fetched.
+     * @return JsonResponse The response containing current player data or an error message.
+     */
     public function currentPlayersData(int $id)
     {
         try {
-            $team = $this->service->getCurrentPlayersData($id);
+            $playersData = $this->service->getCurrentPlayersData($id);
 
-            return $this->responseOk($team);
+            return $this->responseOk($playersData);
+        } catch (\Exception $e) {
+            return $this->responseUnprocessableEntity($e->getMessage());
+        }
+    }
+
+    /**
+     * Retrieves player data for a given team ID.
+     *
+     * This function attempts to fetch all the team players data using the provided
+     * player ID. If successful, it returns a successful response with the
+     * players data. In case of an exception, it handles the error by returning
+     * an unprocessable entity response with the exception message.
+     *
+     * @param int $id The ID of the team whose data is to be retrieved.
+     * @return JsonResponse The response containing player data or an error message.
+     */
+    public function playersData(int $id)
+    {
+        try {
+            $playersData = $this->service->getPlayersData($id);
+
+            return $this->responseOk($playersData);
         } catch (\Exception $e) {
             return $this->responseUnprocessableEntity($e->getMessage());
         }

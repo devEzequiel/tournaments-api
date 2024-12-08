025fd4bea68e598c837f76787ef0badb43699898
Ezequiel Oliveira <esofinney@gmail.com>
2024-12-08 02:01:26 -0300

    Enhance team analytics by adding player statistics.
    Introduced methods to retrieve comprehensive player statistics including goals, assists, and average ratings for both current and past team members. Added exception handling to manage errors when teams or players are not found, ensuring robust error reporting.

--- app/Services/Analytics/TeamAnalyticService.php
+++ app/Services/Analytics/TeamAnalyticService.php
@@ -4,6 +4,7 @@ namespace App\Services\Analytics;
 
 use App\Models\Team;
 use App\Services\BaseService;
+use Exception;
 
 class TeamAnalyticService extends BaseService implements TeamAnalyticContract
 {
@@ -11,15 +12,102 @@ class TeamAnalyticService extends BaseService implements TeamAnalyticContract
     {
         parent::__construct(new Team());
     }
+
+    /**
+     * @throws Exception
+     */
     public function getCurrentPlayersData(int $id)
     {
-        $team_players_data = $this->model::query()->where('team_id', $id)
-            ->get();
 
-        if (!$team_players) {
+        $team = $this->model::find($id);
+        if (!$team) {
+            throw new Exception('Time não encontrado');
+        }
+
+        $team_players_data = $team->players()->with([
+            'fixtures' => function ($query) {
+                $query->whereColumn('home_team_id', '=', 'team_id')
+                    ->orWhereColumn('away_team_id', '=', 'team_id');
+            },
+            'fixtures.goals',
+            'playerRates' => function ($query) {
+                $query->selectRaw('player_id, AVG(rating) as average_rating')
+                    ->groupBy('player_id');
+            }
+        ])->get()->map(function ($player) {
+            $totalGoals = $player->fixtures->sum(function ($fixture) use ($player) {
+                return $fixture->goals->where('scorer_id', $player->id)->count();
+            });
+
+            $totalAssists = $player->fixtures->sum(function ($fixture) use ($player) {
+                return $fixture->goals->where('assist_id', $player->id)->count();
+            });
+
+            $totalMatches = $player->fixtures->count();
+
+            $averagePlayerRate = optional($player->playerRates->first())->average_rating;
+
+            return [
+                'player_id' => $player->id,
+                'player_name' => $player->name,
+                'total_goals' => $totalGoals,
+                'total_assists' => $totalAssists,
+                'total_matches' => $totalMatches,
+                'average_player_rate' => $averagePlayerRate,
+            ];
+        });
+
+        if ($team_players_data->isEmpty()) {
             throw new Exception('Nenhum jogador encontrado');
         }
 
-        return $team_players;
+        return $team_players_data;
+    }
+
+    public function getTeamPlayersData(int $id)
+    {
+        $team = $this->model::find($id);
+
+        $pastAndCurrentPlayersData = $team->with(['players' => function ($query) use ($id) {
+            $query->where('team_player.team_id', $id);
+        }])->with([
+            'players.fixtures' => function ($query) {
+                $query->whereColumn('home_team_id', '=', 'team_id')
+                    ->orWhereColumn('away_team_id', '=', 'team_id');
+            },
+            'players.fixtures.goals',
+            'players.playerRates' => function ($query) {
+                $query->selectRaw('player_id, AVG(rating) as average_rating')
+                    ->groupBy('player_id');
+            }
+        ])->get()->map(function ($player) {
+            $totalGoals = $player->fixtures->sum(function ($fixture) use ($player) {
+                return $fixture->goals->where('scorer_id', $player->id)->count();
+            });
+
+            $totalAssists = $player->fixtures->sum(function ($fixture) use ($player) {
+                return $fixture->goals->where('assist_id', $player->id)->count();
+            });
+
+            $totalMatches = $player->fixtures->count();
+
+            $averagePlayerRate = optional($player->playerRates->first())->average_rating;
+
+            return [
+                'player_id' => $player->id,
+                'player_name' => $player->name,
+                'total_goals' => $totalGoals,
+                'total_assists' => $totalAssists,
+                'total_matches' => $totalMatches,
+                'average_player_rate' => $averagePlayerRate,
+                'current_team' => $player->pivot->current_team,
+            ];
+        });
+
+        if ($pastAndCurrentPlayersData->isEmpty()) {
+            throw new Exception('Nenhum jogador encontrado');
+        }
+
+        return $pastAndCurrentPlayersData;
     }
 }

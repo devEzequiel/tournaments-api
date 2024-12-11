ba0bd24d2581d950e6d34b1822c76da623cd4fd0
Ezequiel Oliveira <esofinney@gmail.com>
2024-12-11 19:28:24 -0300

    Refactor service bindings to use 'App\Modules' namespace

--- routes/api/championship.php
+++ routes/api/championship.php
@@ -1,10 +1,9 @@
 <?php
 
-use App\Http\Controllers\Api\Championship\ChampionshipController;
+use App\Modules\Championship\ChampionshipController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(ChampionshipController::class)->group(function () {
     Route::get('championship/{championship_id}/fixtures', 'getFixtures');
-    Route::get('championship/fixture/{fixture_id}', 'showFixture');
     Route::apiResource('championship', ChampionshipController::class);
 });

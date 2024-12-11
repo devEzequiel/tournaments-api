9c1f29f19f5005011a9fe7c726cfdaa9cfe57977
Ezequiel Oliveira <esofinney@gmail.com>
2024-12-11 19:29:59 -0300

    Rename and move PlayerAnalyticController to new module path

--- routes/api/players.php
+++ routes/api/players.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Api\Player\PlayerController;
+use App\Modules\Player\PlayerController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(PlayerController::class)->group(function () {

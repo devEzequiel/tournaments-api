34cb64536d25ffaa1367a1d2dba281ea29eada84
esofi <esofinney@gmail.com>
2026-02-03 22:14:07 -0300

    refactor route structure for authentication and settings

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,21 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+// Rotas de autenticação
+require_once ('auth.php');
+
+// Redireciona para a página inicial
 Route::get('/', function () {
-    return redirect('/championships'); // Redireciona para a página inicial
+    return redirect('/championships');
 });
 
-require_once ('web/championships.php');
-require_once ('web/teams.php');
-require_once ('web/players.php');
+// Rotas protegidas por autenticação
+Route::middleware('auth')->group(function () {
+    require_once ('web/championships.php');
+    require_once ('web/teams.php');
+    require_once ('web/players.php');
 
-Route::get('/settings', function () {
-    return Inertia::render('Settings'); // Aqui renderizará a página de Settings
-})->name('settings');
+    Route::get('/settings', function () {
+        return Inertia::render('Settings');
+    })->name('settings');
+});

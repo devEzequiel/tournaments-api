815c531b2e0a4b358c9cfaa253288658eff6b2bd
esofi <esofinney@gmail.com>
2026-02-08 20:42:19 -0300

    refactor TeamContract interface to enhance method documentation

--- app/Contracts/TeamContract.php
+++ app/Contracts/TeamContract.php
@@ -2,11 +2,52 @@
 
 namespace App\Contracts;
 
+/**
+ * Interface que define o contrato para serviços de Time.
+ * 
+ * Estabelece os métodos obrigatórios para manipulação de times,
+ * garantindo consistência entre diferentes implementações.
+ */
 interface TeamContract
 {
+    /**
+     * Busca um time pelo ID.
+     * 
+     * @param int $id ID do time
+     * @return mixed Time encontrado
+     * @throws \Exception Quando não encontrado
+     */
     public function find (int $id);
+
+    /**
+     * Cria um novo time.
+     * 
+     * @param mixed $data Dados do time (name, colors)
+     * @return bool Sucesso da operação
+     */
     public function create ($data);
+
+    /**
+     * Retorna todos os times cadastrados.
+     * 
+     * @return mixed Coleção de times
+     */
     public function all();
+
+    /**
+     * Atualiza um time existente.
+     * 
+     * @param mixed $data Dados para atualização
+     * @param mixed $id ID do time
+     * @return bool Sucesso da operação
+     */
     public function update ($data, $id);
+
+    /**
+     * Remove um time.
+     * 
+     * @param mixed $id ID do time
+     * @return bool Sucesso da operação
+     */
     public function delete ($id);
 }
